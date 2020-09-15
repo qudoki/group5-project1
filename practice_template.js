d32853fcbd4fb88d4fe938db24c12281043f992c
blalbeharry <67478635+blalbeharry@users.noreply.github.com>
2020-09-14 21:44:13 -0400

    Add files via upload

---
+++ practice_template.js
@@ -0,0 +1,21 @@
+ // user interface with various buttons representing different emotions
+            // variable 1 emotion 
+            // variable 2 emotion 
+            // variable 3 emotion 
+            // variable 4 emotion 
+            // variable 5 emotion 
+            // variable 6 emotion 
+            // variable 7 emotion 
+            // variable 8 emotion 
+            // variable 9 emotion 
+            // variable 10 emotion 
+
+        // dynamic div 
+            // top section of div with span varying images for current song
+            // bottom div will be music player
+                // music player functionality
+                    // pull song from array of music matching criteria 
+                    // fetch API/ API call for information pertaining to song of specific category
+
+                // if player populates on page include variables for play/ pause, forward and backward. 
+                
