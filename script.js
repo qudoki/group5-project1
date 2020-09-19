39ad894951d3136527c949e3da0c7159293095cc
Skyler R <skyler.rencher@gmail.com>
2020-09-19 02:09:06 -0400

    added the ajax call and some console logs to check to see if the call was responsive. added the link to the script and the jquery cdn inside of the index.html.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $('document').ready(function(){
     
 
 var spotifyAPIKey = "BQD-B3lXyyP4fS9VMdrgVGJRJZB0haUcTt5Nfe0nGCPCF-OkM8tGcqWgvsYJ6rSD5Psqg7r3Zb6z6vCQF62hu5gLVSZ8F8CEuPZNmfbsyPnJ2hvEeipyb23NcPa7LbUKH2KWS8ADBAqZY-R1cF1JwnpdTSwmoQ"
-var giphyAPIKey = "KNlTgJabkqAK66NkPnBuEEWTVntHdFAb"
+var giphyAPIKey = "api_key:KNlTgJabkqAK66NkPnBuEEWTVntHdFAb"
 
 
 
@@ -28,67 +28,83 @@ var giphyAPIKey = "KNlTgJabkqAK66NkPnBuEEWTVntHdFAb"
 
 
     // Spotify Playback SDK
-    window.onSpotifyWebPlaybackSDKReady = () => {
-        const token = '[My Spotify Web API access token]';
-        const player = new Spotify.Player({
-          name: 'Web Playback SDK Quick Start Player',
-          getOAuthToken: cb => { cb(token); }
-        });
-        // Error handling
-        player.addListener('initialization_error', ({ message }) => { console.error(message); });
-        player.addListener('authentication_error', ({ message }) => { console.error(message); });
-        player.addListener('account_error', ({ message }) => { console.error(message); });
-        player.addListener('playback_error', ({ message }) => { console.error(message); });
-        // Playback status updates
-        player.addListener('player_state_changed', state => { console.log(state); });
-        // Ready
-        player.addListener('ready', ({ device_id }) => {
-          console.log('Ready with Device ID', device_id);
-        });
-        // Not Ready
-        player.addListener('not_ready', ({ device_id }) => {
-          console.log('Device ID has gone offline', device_id);
-        });
-        // Connect to the player!
-        player.connect();
-      };
+    // window.onSpotifyWebPlaybackSDKReady = () => {
+    //     const token = '[My Spotify Web API access token]';
+    //     const player = new Spotify.Player({
+    //       name: 'Web Playback SDK Quick Start Player',
+    //       getOAuthToken: cb => { cb(token); }
+    //     });
+    //     // Error handling
+    //     player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    //     player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    //     player.addListener('account_error', ({ message }) => { console.error(message); });
+    //     player.addListener('playback_error', ({ message }) => { console.error(message); });
+    //     // Playback status updates
+    //     player.addListener('player_state_changed', state => { console.log(state); });
+    //     // Ready
+    //     player.addListener('ready', ({ device_id }) => {
+    //       console.log('Ready with Device ID', device_id);
+    //     });
+    //     // Not Ready
+    //     player.addListener('not_ready', ({ device_id }) => {
+    //       console.log('Device ID has gone offline', device_id);
+    //     });
+    //     // Connect to the player!
+    //     player.connect();
+    //   };
 
-    //   Spotify Fetch Request:
-    const play = ({
-        spotify_uri,
-        playerInstance: {
-          _options: {
-            getOAuthToken,
-            id
-          }
-        }
-      }) => {
-        getOAuthToken(access_token => {
-          fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({ uris: [spotify_uri] }),
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${access_token}`
-            },
-          });
-        });
-      };
-      play({
-        playerInstance: new Spotify.Player({ name: "..." }),
-        spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
-      });
+    // //   Spotify Fetch Request:
+    // const play = ({
+    //     spotify_uri,
+    //     playerInstance: {
+    //       _options: {
+    //         getOAuthToken,
+    //         id
+    //       }
+    //     }
+    //   }) => {
+    //     getOAuthToken(access_token => {
+    //       fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+    //         method: 'PUT',
+    //         body: JSON.stringify({ uris: [spotify_uri] }),
+    //         headers: {
+    //           'Content-Type': 'application/json',
+    //           'Authorization': `Bearer ${access_token}`
+    //         },
+    //       });
+    //     });
+    //   };
+    //   play({
+    //     playerInstance: new Spotify.Player({ name: "..." }),
+    //     spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+    //   });
 
     
     // Second API - Visializer? (GIF , video)
     // API Ajax Request - Server Side API's
 
     // GIPHY API  
-    // $.ajax(giphyGIF).done(function (response) {
-    // 	console.log(response);
-    // });
+
+    var giphyURL = "api.giphy.com/v1/gifs/random";
+    var happyURL = giphyURL + giphyAPIKey + "tag:happy";
+
+    $(happyBtn).on("click", function(event) {
+      event.preventDefault();
+
+      $.ajax({
+          url: happyURL,
+          method: "GET"
+      })
 
     // .then statement to attach to the html elements
+    .then(function(response) {
+      console.log(happyURL);
+      console.log(response);
+
+
+    });
+
+  });
 
 
 
